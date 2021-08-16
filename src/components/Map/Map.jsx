1801c4339cf21c7d705127b62c01c76d8711547f
dandrok <patryk.stusik@gmail.com>
2021-08-17 00:08:19 +0200

    create header and map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,6 +10,8 @@ const Map = () => {
   const classes = useStyles()
   const isMobile = useMediaQuery('(min-width:600px)')
 
+  const coordinates = { lat: 0, lng: 0 }
+
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
