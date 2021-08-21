7a0333e9db3ded9843922556aaf033282d242075
dandrok <patryk.stusik@gmail.com>
2021-08-21 23:58:18 +0200

    display info about on map

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ function App() {
   const [coordinates, setCoordinates] = useState({})
   const [bounds, setBounds] = useState({})
 
+  const [childClicked, setChildClicked] = useState(null)
+
+  const [isLoading, setIsLoading] = useState(false)
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       ({ coords: { latitude, longitude } }) => {
@@ -21,8 +25,11 @@ function App() {
   }, [])
 
   useEffect(() => {
+    setIsLoading(true)
+
     getPlacesData(bounds.sw, bounds.ne).then((data) => {
       setPlaces(data)
+      setIsLoading(false)
     })
   }, [coordinates, bounds])
 
@@ -32,13 +39,19 @@ function App() {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
