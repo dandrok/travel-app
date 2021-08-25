e0e02e2243588f12f2072fbd180af9e23a5d0ee8
dandrok <patryk.stusik@gmail.com>
2021-08-25 23:34:53 +0200

    style map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,6 +4,7 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 import Rating from '@material-ui/lab/Rating'
 
+import mapStyles from './mapStyles.json'
 import useStyles from './styles'
 
 const Map = ({
@@ -24,7 +25,11 @@ const Map = ({
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng })
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
